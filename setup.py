3b5616d6f04b16305ec0fa8ae964e80986dac003
Katherine Rose <kiarc@kiarc.win>
2024-08-31 17:14:34 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,6 +3,11 @@ from numpy import get_include
 
 
 setup(
+    name="gentrig",
+    version="0.1.0",
+    description="Trigonometric functions generalized for parabolas and polygons",
+    author="Katherine Rose",
+    author_email="kiarc@kiarc.win",
     ext_modules=[
         Extension(name="gentrig", sources=["gentrig.c"], include_dirs=[get_include()])
     ]
