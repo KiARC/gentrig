43595369ec4d6abd26aec4f68dd8ce15d21662dc
Katherine Rose <kiarc@kiarc.win>
2024-08-31 22:04:14 -0400

    Remove long_description from setup.py
    It should be generated from `readme`

--- setup.py
+++ setup.py
@@ -3,4 +3,4 @@ from numpy import get_include
 
 gentrig_ext = Extension(name="gentrig", sources=["gentrig.c"], include_dirs=[get_include()])
 
-setup(ext_modules=[gentrig_ext], long_description=open('README.md').read(), long_description_content_type='text/markdown')
+setup(ext_modules=[gentrig_ext])
